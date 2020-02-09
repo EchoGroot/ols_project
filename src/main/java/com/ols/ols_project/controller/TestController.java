06d373178ff85fc51aab17c5182b377f73483f48
EchoGroot <326018984@qq.com>
2020-02-10 00:12:34 +0800

    mysql，redis配置成功

--- src/main/java/com/ols/ols_project/controller/TestController.java
+++ src/main/java/com/ols/ols_project/controller/TestController.java
@@ -1,8 +1,16 @@
 package com.ols.ols_project.controller;
 
+import com.ols.ols_project.config.RedisConfig;
+import com.ols.ols_project.model.TestUser;
+import com.ols.ols_project.service.TestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 /**
  * @author yuyy
@@ -10,9 +18,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class TestController {
+
+    @Autowired
+    private TestService testService;
+
+    @Autowired
+    private RedisConfig redisConfig;
+
     @RequestMapping("testindex")
     public String showIndex(Model model){
         model.addAttribute("msg","ols");
+        List<TestUser> users = testService.selAll();
+        model.addAttribute("list",users);
+        System.out.println(users);
+        //Redis测试
+        RedisTemplate<Object, Object> redisTemplate = redisConfig.getRedisTemplate();
+        //配置value的Json格式存储
+        redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(TestUser.class));
+
+        redisTemplate.opsForValue().set("testkey",new TestUser());
+
+
+        redisTemplate.opsForValue().set("dd","fdsfdsfsdfds");
+        TestUser value = (TestUser)redisTemplate.opsForValue().get("testkey");
+        System.out.println(value);
         return "TestIndex";
     }
 }
--- src/main/java/com/ols/ols_project/service/Impl/TestServiceImpl.java
+++ src/main/java/com/ols/ols_project/service/Impl/TestServiceImpl.java
@@ -1,7 +1,10 @@
 package com.ols.ols_project.service.Impl;
 
+import com.ols.ols_project.mapper.TestMapper;
 import com.ols.ols_project.model.TestUser;
 import com.ols.ols_project.service.TestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
@@ -9,9 +12,13 @@ import java.util.List;
  * @author yuyy
  * @date 20-2-9 下午8:57
  */
+@Service
 public class TestServiceImpl implements TestService {
+
+    @Autowired
+    private TestMapper testMapper;
     @Override
     public List<TestUser> selAll() {
-        return null;
+        return testMapper.selAll();
     }
 }
