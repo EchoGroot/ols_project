b3bd5bf0f69e408739f31357741a22ba1e137597
EchoGroot <326018984@qq.com>
2020-02-17 11:45:15 +0800

    解决关于freemarker的一个异常

--- src/main/java/com/ols/ols_project/OlsProjectApplication.java
+++ src/main/java/com/ols/ols_project/OlsProjectApplication.java
@@ -3,9 +3,10 @@ package com.ols.ols_project;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.freemarker.FreeMarkerAutoConfiguration;
 
 @MapperScan("com.ols.ols_project.mapper")
-@SpringBootApplication
+@SpringBootApplication(exclude = { FreeMarkerAutoConfiguration.class })
 public class OlsProjectApplication {
 
     public static void main(String[] args) {
