87687c1287cb79a6dc5c07bd6d44915f02a97f55
EchoGroot <326018984@qq.com>
2020-03-05 17:42:48 +0800

    解决SpringBoot2静态资源访问404

--- src/main/java/com/ols/ols_project/OlsProjectApplication.java
+++ src/main/java/com/ols/ols_project/OlsProjectApplication.java
@@ -4,17 +4,24 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.freemarker.FreeMarkerAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * springboot启动类
+ * 继承SpringBootServletInitializer是为了打包
  */
 @MapperScan("com.ols.ols_project.mapper")
 //解决一个关于freemark的异常
 @SpringBootApplication(exclude = { FreeMarkerAutoConfiguration.class })
-public class OlsProjectApplication {
+public class OlsProjectApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(OlsProjectApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(OlsProjectApplication.class);
+    }
 }
