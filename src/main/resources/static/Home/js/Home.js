87e6f3c85f476be42c355dbb48ad570654c71042
wjp919497158 <919497158@qq.com>
2020-03-31 16:15:04 +0800

    封装表格重载时的参数，修改页面布局BUG

--- src/main/resources/static/Home/js/Home.js
+++ src/main/resources/static/Home/js/Home.js
@@ -7,42 +7,42 @@ var query=getQueryVariable('query'); //查询参数  needlable和adopted两种
 var url='/task/getAllTask'
 $(function () {
     // layui初始化
-    layui.use(['layer', 'form','table'], function() {
+    layui.use(['layer', 'form', 'table'], function () {
         var table = layui.table;
         var layer = layui.layer;
         var form = layui.form;
         // 发布的任务
-        cols=[[ //表头
-            {field: 'id', title: '任务编号', align:'center',width: '10%',fixed: 'left'}
-            , {field: 'name', title: '任务名称', align:'center',width: '10%'}
-            , {field: 'points', title: '任务分值', align:'center',width: '10%', sort: true}
-            , {field: 'type', title: '文件类型', align:'center',width: '10%', sort: true}
-            , {field: 'release_time', title: '发布时间',align:'center', width: '15%', sort: true}
-            , {field: 'accept_num', title: '接受者数量',align:'center', width: '10%', sort: true}
-            , {field: 'ext1', title: '完成任务数量',align:'center', width: '10%', sort: true}
-            , {title: '操作', align:'center',toolbar: '#barHandle'}
+        cols = [[ //表头
+            {field: 'id', title: '任务编号', align: 'center', width: '10%', fixed: 'left'}
+            , {field: 'name', title: '任务名称', align: 'center', width: '10%'}
+            , {field: 'points', title: '任务分值', align: 'center', width: '10%', sort: true}
+            , {field: 'type', title: '文件类型', align: 'center', width: '10%', sort: true}
+            , {field: 'release_time', title: '发布时间', align: 'center', width: '15%', sort: true}
+            , {field: 'accept_num', title: '接受者数量', align: 'center', width: '10%', sort: true}
+            , {field: 'ext1', title: '完成任务数量', align: 'center', width: '10%', sort: true}
+            , {title: '操作', align: 'center', toolbar: '#barHandle'}
         ]];
         // 渲染表格 *****由于默认界面是tableIns，所以选择先渲染tableIns2再渲染tableIns覆盖。
-        var tableIns2=table.render({
+        var tableIns2 = table.render({
             elem: '#taskList'
             , height: 700
             , url: url //数据接口
             , page: true //开启分页
-            , limits: [15,30,50,100]
+            , limits: [15, 30, 50, 100]
             , limit: 15
             , method: 'get'
             , autoSort: false
-            , where:{
-                query:'adopted',
-                queryInfo:'',
-                searchInfo:'',
-                field:'',
-                order:''
+            , where: {
+                query: 'adopted',
+                queryInfo: '',
+                searchInfo: '',
+                field: '',
+                order: ''
             }, page: {
                 curr: 1 //重新从第 1 页开始
             }
             // 渲染表格结束后的回调函数
-            , parseData: function(res) { //res 即为原始返回的数据
+            , parseData: function (res) { //res 即为原始返回的数据
                 return {
                     "code": res.meta.status, //解析接口状态
                     "msg": res.meta.msg, //解析提示文本
@@ -50,40 +50,40 @@ $(function () {
                     "data": res.data.taskList //解析数据列表
                 }
             }
-            , cols:[[ //表头
-                {field: 'id', title: '任务编号', align:'center',width: '10%',fixed: 'left'}
-                , {field: 'name', title: '任务名称', align:'center',width: '10%'}
-                , {field: 'points', title: '任务分值', align:'center',width: '10%', sort: true}
-                , {field: 'type', title: '文件类型', align:'center',width: '10%', sort: true}
-                , {field: 'finish_time', title: '完成时间',align:'center', width: '15%', sort: true}
-                , {title: '操作', align:'center',toolbar: '#barHandle'}
+            , cols: [[ //表头
+                {field: 'id', title: '任务编号', align: 'center', width: '10%', fixed: 'left'}
+                , {field: 'name', title: '任务名称', align: 'center', width: '10%'}
+                , {field: 'points', title: '任务分值', align: 'center', width: '10%', sort: true}
+                , {field: 'type', title: '文件类型', align: 'center', width: '10%', sort: true}
+                , {field: 'finish_time', title: '完成时间', align: 'center', width: '15%', sort: true}
+                , {title: '操作', align: 'center', toolbar: '#barHandle'}
             ]]
-            ,done: function(res, curr, count){
-                if(page==='acceptTask'){
+            , done: function (res, curr, count) {
+                if (page === 'acceptTask') {
                     $(".adoptClass").hide();
                 }
             }
         });
-        var tableIns=table.render({
+        var tableIns = table.render({
             elem: '#taskList'
             , height: 700
             , url: url //数据接口
             , page: true //开启分页
-            , limits: [15,30,50,100]
+            , limits: [15, 30, 50, 100]
             , limit: 15
             , method: 'get'
             , autoSort: false
-            , where:{
-                query:'needlable',
-                queryInfo:'',
-                searchInfo:'',
-                field:'',
-                order:''
+            , where: {
+                query: 'needlable',
+                queryInfo: '',
+                searchInfo: '',
+                field: '',
+                order: ''
             }, page: {
                 curr: 1 //重新从第 1 页开始
             }
             // 渲染表格结束后的回调函数
-            , parseData: function(res) { //res 即为原始返回的数据
+            , parseData: function (res) { //res 即为原始返回的数据
                 return {
                     "code": res.meta.status, //解析接口状态
                     "msg": res.meta.msg, //解析提示文本
@@ -92,137 +92,129 @@ $(function () {
                 }
             }
             , cols: cols
-            ,done: function(res, curr, count){
-                if(page==='acceptTask'){
+            , done: function (res, curr, count) {
+                if (page === 'acceptTask') {
                     $(".adoptClass").hide();
                 }
             }
 
         });
         //监听工具条
-        table.on('tool(monitorToolbar)', function(obj){ //注：tool 是工具条事件名，test 是 table 原始容器的属性 lay-filter="对应的值"
+        table.on('tool(monitorToolbar)', function (obj) { //注：tool 是工具条事件名，test 是 table 原始容器的属性 lay-filter="对应的值"
             var data = obj.data; //获得当前行数据
             var layEvent = obj.event; //获得 lay-event 对应的值（也可以是表头的 event 参数对应的值）
             // 工具条的点击事件 点击量+1 并跳转详情页面
             $.ajax({
                 type: "post",
-                url:"/task/clickNumPlus",
-                data:{
-                    taskId:data.id
+                url: "/task/clickNumPlus",
+                data: {
+                    taskId: data.id
                 }
             })
             checkFunc(data.id);
         });
         //监听表头 用来排序
-        table.on('sort(monitorToolbar)', function(obj){ //注：tool是工具条事件名，test是table原始容器的属性 lay-filter="对应的值"
+        table.on('sort(monitorToolbar)', function (obj) { //注：tool是工具条事件名，test是table原始容器的属性 lay-filter="对应的值"
             //监听排序
             console.log(obj.field); //当前排序的字段名
             console.log(obj.type); //当前排序类型：desc（降序）、asc（升序）、null（空对象，默认排序）
             console.log(this);//当前排序的 th 对象*/;
             console.log(page);
-            if(page != ''){
+            if (page != '') {
                 tableIns.reload({
                     initSort: obj //记录初始排序，如果不设的话，将无法标记表头的排序状态。 layui 2.1.1 新增参数
-                    ,where: { //请求参数（注意：这里面的参数可任意定义，并非下面固定的格式）
+                    , where: { //请求参数（注意：这里面的参数可任意定义，并非下面固定的格式）
                         field: obj.field,
                         order: obj.type
-                    },page: {
+                    }, page: {
                         curr: 1 //重新从第 1 页开始
                     }
                 });
-            }else {
+            } else {
                 tableIns2.reload({
                     initSort: obj //记录初始排序，如果不设的话，将无法标记表头的排序状态。 layui 2.1.1 新增参数
-                    ,where: { //请求参数（注意：这里面的参数可任意定义，并非下面固定的格式）
+                    , where: { //请求参数（注意：这里面的参数可任意定义，并非下面固定的格式）
                         field: obj.field,
                         order: obj.type
-                    },page: {
+                    }, page: {
                         curr: 1 //重新从第 1 页开始
                     }
                 });
             }
         });
         $("#queryReleasedTask").click(function () {
-            page = 'releaseNotFinishTask';
-            $("#searchInput").val('');
-            $("#queryReleasedTask").attr('class',"layui-this");
-            $("#queryImgTask").attr('class',"");
-            $("#queryTxtTask").attr('class',"");
-            $("#queryAdoptedTask").attr('class',"");
+            paramConfig();
+            $("#queryReleasedTask").attr('class', "layui-this");
             tableIns.reload({
-                where:{
-                    query:'needlable',
-                    queryInfo:'',
-                    searchInfo:'',
-                    field:'',
-                    order:''
+                where: {
+                    query: 'needlable',
+                    queryInfo: '',
+                    searchInfo: '',
+                    field: '',
+                    order: ''
                 }
             });
         })
         $("#queryImgTask").click(function () {
-            $("#searchInput").val('');
-            page = 'releaseNotFinishTask';
-            $("#queryReleasedTask").attr('class',"");
-            $("#queryImgTask").attr('class',"layui-this");
-            $("#queryTxtTask").attr('class',"");
-            $("#queryAdoptedTask").attr('class',"");
+            paramConfig();
+            $("#queryImgTask").attr('class', "layui-this");
             tableIns.reload({
-                where:{
-                    query:'needlable',
-                    queryInfo:'img',
-                    searchInfo:'',
-                    field:'',
-                    order:''
+                where: {
+                    query: 'needlable',
+                    queryInfo: 'img',
+                    searchInfo: '',
+                    field: '',
+                    order: ''
                 }
             });
         })
         $("#queryTxtTask").click(function () {
-            $("#searchInput").val('');
-            page = 'releaseNotFinishTask';
-            $("#queryReleasedTask").attr('class',"");
-            $("#queryImgTask").attr('class',"");
-            $("#queryTxtTask").attr('class',"layui-this");
-            $("#queryAdoptedTask").attr('class',"");
+            paramConfig();
+            $("#queryTxtTask").attr('class', "layui-this");
             tableIns.reload({
-                where:{
-                    query:'needlable',
-                    queryInfo:'doc',
-                    searchInfo:'',
-                    field:'',
-                    order:''
+                where: {
+                    query: 'needlable',
+                    queryInfo: 'doc',
+                    searchInfo: '',
+                    field: '',
+                    order: ''
                 }
             });
         })
 
         $("#queryAdoptedTask").click(function () {
-            page = '';//************--------------------****************/
-            $("#searchInput").val('');
-            $("#queryReleasedTask").attr('class', "");
-            $("#queryImgTask").attr('class', "");
-            $("#queryTxtTask").attr('class', "");
+            paramConfig();
+            page = '';
             $("#queryAdoptedTask").attr('class', "layui-this");
-            tableIns2.reload();
+            tableIns2.reload({
+                where: {
+                    query: 'adopted',
+                    queryInfo: '',
+                    searchInfo: '',
+                    field: '',
+                    order: ''
+                }
+            })
         })
         //搜索按钮点击事件
         $("#searchButton").click(function () {
-            if(page != ''){
+            if (page != '') {
                 tableIns.reload({
-                    where:{
-                        searchInfo:$("#searchInput").val(),
-                        field:'',
-                        order:''
+                    where: {
+                        searchInfo: $("#searchInput").val(),
+                        field: '',
+                        order: ''
                     },
                     page: {
                         curr: 1 //重新从第 1 页开始
                     }
                 });
-            }
-            else {
+            } else {
                 tableIns2.reload({
-                    where:{
-                        searchInfo:$("#searchInput").val(),
-                        field:'',
-                        order:''
+                    where: {
+                        searchInfo: $("#searchInput").val(),
+                        field: '',
+                        order: ''
                     },
                     page: {
                         curr: 1 //重新从第 1 页开始
@@ -233,34 +225,40 @@ $(function () {
     });
     judgeLogin();
     $("#newTaskBtn").click(function () {
-        if(userId!=null){
-            $.ajax({
-                url: '/user/judgeLogin',
-                type: "GET",
-                data: {
-                    "userId": userId
-                },
-                success: function (resultData) {
-                    resultData = JSON.parse(resultData);
-                    if(resultData.meta.status === "200"){
-                        window.location.href='/Home/newTask.html?'+"userId="+userId;
-                    }else{
-                        window.sessionStorage.setItem(
-                            'gotoUrl','/Home/newTask.html?'+"userId="
-                        );
-                        window.location.href='/Home/login.html';//登陆完了直接跳转新任务
-                    }
+        gotoUrlByJudege('/Home/newTask.html?' + "userId=")
+    });
+
+})
+//跳转页面要求已登陆状态的使用此方法
+function gotoUrlByJudege(str) { //str为目的跳转地址
+    if(userId!=null){
+        $.ajax({
+            url: '/user/judgeLogin',
+            type: "GET",
+            data: {
+                "userId": userId
+            },
+            success: function (resultData) {
+                resultData = JSON.parse(resultData);
+                if(resultData.meta.status === "200"){
+                    window.location.href=str+userId;
+                }else{
+                    window.sessionStorage.setItem(
+                        'gotoUrl',str  //str1 2 3
+                    );
+                    window.location.href='/Home/login.html';
                 }
-            })
-        }
-        else {
-            window.sessionStorage.setItem(
-                'gotoUrl','/Home/newTask.html?'+"userId="
-            );
-            window.location.href='/Home/login.html';//登陆完了直接跳转新任务
-        }
-    })
-});
+            }
+        })
+    }
+    else {
+        window.sessionStorage.setItem(
+            'gotoUrl',str
+        );
+        window.location.href='/Home/login.html';//登陆完了直接跳转新任务
+    }
+}
+
 //获取URL参数
 function getQueryVariable(name) {
     var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
@@ -270,6 +268,15 @@ function getQueryVariable(name) {
         return window.sessionStorage.getItem(name);
     }
     return null;
+}
+//标题点击后参数重置
+function paramConfig() {
+    page = 'releaseNotFinishTask';
+    $("#searchInput").val('');
+    $("#queryReleasedTask").attr('class', "");
+    $("#queryImgTask").attr('class', "");
+    $("#queryTxtTask").attr('class', "");
+    $("#queryAdoptedTask").attr('class', "");
 }
 
 // 查看任务
@@ -332,9 +339,9 @@ function judgeLogin() {
                                 var div1=document.getElementById("login");
                                 div1.style.display="block";
                                 var li1=document.getElementById("acceptli");
-                                li1.style.display="block";
+                                li1.style.visibility="visible";
                                 var li2=document.getElementById("releaseli");
-                                li2.style.display="block";
+                                li2.style.visibility="visible"; //这样做布局没问题了，但是存在BUG 可以前端修改显示出来。所以点击事件需要判断登录状态。
                                 var a=document.getElementById("userName");
                                 a.innerText=name;
                                 a.href="/PersonalCenterPage/index.html?userId="+userId+"&page=personalInfo";
@@ -344,8 +351,9 @@ function judgeLogin() {
                     });
 
                 }else{
-                    //window.location.href='/Home/Home.html';
                     sessionStorage.clear();   //清除所有session值
+                    window.location.href='/Home/Home.html';
+
                 }
 
             }
@@ -357,3 +365,4 @@ function cancel() {
     sessionStorage.clear();   //清除所有session值
     window.location.href='/Home/Home.html';
 }
+
